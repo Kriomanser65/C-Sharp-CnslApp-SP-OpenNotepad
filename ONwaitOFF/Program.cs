6694e85fb78f512e095120500eb2e4744d175350
Stekoks <Stekoks@DESKTOP-4SK39GD>
2023-11-27 15:55:24 +0200

    Add project files.

---
+++ ONwaitOFF/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ONwaitOFF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process process = new Process();
+            try
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = @"C:\\Windows\\System32\\notepad.exe",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                };
+                    process.StartInfo = startInfo;
+                process.Start();
+                process.WaitForExit();
+                int exitCode = process.ExitCode;
+                Console.WriteLine($"Complete: {exitCode}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                if (process != null)
+                {
+                    process.Close();
+                    process.Dispose();
+                }
+            }
+        }
+    }
+}
